c1e9e4fd111a93a73867f9ed7ab4b69c35571277
Joao marcos <jmarcosdo@gmail.com>
2016-06-30 19:59:53 -0300

    novas bucas

--- src/entity/Node.java
+++ src/entity/Node.java
@@ -1,19 +1,21 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by jm on 5/28/16.
  */
-public class Node {
+public class Node implements Comparable<Node> {
 	public String key;
 	public List<Integer> estado;
 	public Node pai;
 	public List<Node> filhos;
 	public int possitionNull;
 	public int level;
+	public int val = 0;
 
 	public Node(Node pai, List<Integer> estado) {
 		this.pai = pai;
@@ -21,6 +23,7 @@ public class Node {
 		this.key = createKey();
 		possitionNull = estado.indexOf(-1);
 		this.filhos = new ArrayList<Node>();
+		this.level = pai != null ? pai.level + 1 : 0;
 	}
 
 	/**
@@ -56,29 +59,31 @@ public class Node {
 			keyaux = createKey(list_aux);
 			if (!hashMap.containsKey(keyaux)) {
 				this.filhos.add(new Node(this, createEstado(possitionNull, possitionNull - 2)));
+				return true;
 			}
-			return true;
+
 		} else if (regra == 1 && possitionNull > 0) {
 			list_aux = createEstado(possitionNull, possitionNull - 1);
 			keyaux = createKey(list_aux);
 			if (!hashMap.containsKey(keyaux)) {
 				this.filhos.add(new Node(this, createEstado(possitionNull, possitionNull - 1)));
+				return true;
+
 			}
-			return true;
 		} else if (regra == 2 && possitionNull < estado.size() - 1) {
 			list_aux = createEstado(possitionNull, possitionNull + 1);
 			keyaux = createKey(list_aux);
 			if (!hashMap.containsKey(keyaux)) {
 				this.filhos.add(new Node(this, createEstado(possitionNull, possitionNull + 1)));
+				return true;
 			}
-			return true;
 		} else if (regra == 3 && possitionNull < estado.size() - 2) {
 			list_aux = createEstado(possitionNull, possitionNull + 2);
 			keyaux = createKey(list_aux);
 			if (!hashMap.containsKey(keyaux)) {
 				this.filhos.add(new Node(this, createEstado(possitionNull, possitionNull + 2)));
+				return true;
 			}
-			return true;
 		}
 		return false;
 	}
@@ -130,34 +135,164 @@ public class Node {
 		return list;
 	}
 
-	public void buscaLargura(HashMap<String, Integer> hashMap, List<Node> list) {
-		System.out.println(key);
+	public Node buscaLargura(HashMap<String, Integer> hashMap, List<Node> list) {
 		if (this.key.equals(Tree.objetivo)) {
-			System.out.println("achou");
-			return;
+			return this;
 		}
 		criarAll(hashMap);
 		for (Node n : filhos) {
 			list.add(n);
 			hashMap.put(n.key, 0);
+		}
+		if (list.size() == 0) {
+			return null;
 		}
 		Node n = list.remove(0);
-		n.buscaLargura(hashMap, list);
+		hashMap.put(n.key,1);
+		return n.buscaLargura(hashMap, list);
 	}
 
-
-	public void buscaProfundidade(HashMap<String, Integer> hashMap, List<Node> list) {
-		System.out.println(key);
+	public Node buscaBacktracking(HashMap<String, Integer> hashMap, List<Node> list) {
 		if (this.key.equals(Tree.objetivo)) {
-			System.out.println("achou");
-			return;
+			return this;
+		}
+		int c=-1;
+		Node n=null;
+		for (int i = 0; i < 4; i++) {
+			if(criarFilhos(i, hashMap)){
+				c++;
+				hashMap.put(filhos.get(c).key, 1);
+				n = filhos.get(c).buscaBacktracking(hashMap, list);
+				if( n!=null ){
+					return n;
+				}
+			}
+		}
+		return null;
+	}
+	public Node buscaProfundidade(HashMap<String, Integer> hashMap, List<Node> list) {
+		if (this.key.equals(Tree.objetivo)) {
+			return this;
 		}
 		criarAll(hashMap);
 		for (Node n : filhos) {
-			list.add(n);
-			hashMap.put(n.key, 0);
+			if (!hashMap.containsKey(n.key)) {
+				list.add(n);
+				hashMap.put(n.key, 0);
+			}
+		}
+		if (list.size() == 0) {
+			return null;
 		}
 		Node n = list.remove(list.size() - 1);
-		n.buscaProfundidade(hashMap, list);
+		hashMap.put(n.key,1);
+		return n.buscaProfundidade(hashMap, list);
+	}
+
+	public Node buscaOrdenada(HashMap<String, Integer> hashMap, List<Node> list) {
+		if (this.key.equals(Tree.objetivo)) {
+			return this;
+		}
+		criarAll(hashMap);
+		for (Node n : filhos) {
+			if (!hashMap.containsKey(n.key)) {
+				n.val = n.pai.val + 1;
+				list.add(n);
+				hashMap.put(n.key, 0);
+			}
+		}
+		if (list.size() == 0) {
+			return null;
+		}
+		Collections.sort(list);
+		Node n = list.remove(0);
+		hashMap.put(n.key,1);
+		return n.buscaOrdenada(hashMap, list);
+	}
+
+	public Node buscaA(HashMap<String, Integer> hashMap, List<Node> list) {
+		if (this.key.equals(Tree.objetivo)) {
+			return this;
+		}
+		criarAll(hashMap);
+		for (Node n : filhos) {
+			if (!hashMap.containsKey(n.key)) {
+				n.val = n.level + n.getValorHeuristica();
+				list.add(n);
+				hashMap.put(n.key, 0);
+			}
+		}
+		if (list.size() == 0) {
+			return null;
+		}
+		Collections.sort(list);
+		Node n = list.remove(0);
+		hashMap.put(n.key,1);
+		return n.buscaA(hashMap, list);
+	}
+
+	public Node buscaIDA(HashMap<String, Integer> hashMap, List<Node> list) {
+		if (this.key.equals(Tree.objetivo)) {
+			return this;
+		}
+		criarAll(hashMap);
+		for (Node n : filhos) {
+			n.val = n.level + n.getValorHeuristica();
+			if (n.val <= Tree.oldIDAPatamar ) {
+				list.add(n);
+				hashMap.put(n.key, 0);
+			}else if(n.val<Tree.newIDAPatamar){
+				Tree.newIDAPatamar=n.val;
+			}
+		}
+		if (list.size() == 0) {
+			return null;
+		}
+		Collections.sort(list);
+		Node n = list.remove(0);
+		hashMap.put(n.key,1);
+		return n.buscaIDA(hashMap, list);
+	}
+
+	public Node buscaGulosa(HashMap<String, Integer> hashMap, List<Node> list) {
+		if (this.key.equals(Tree.objetivo)) {
+			return this;
+		}
+		criarAll(hashMap);
+		for (Node n : filhos) {
+			if (!hashMap.containsKey(n.key)) {
+				n.val = n.getValorHeuristica();
+				list.add(n);
+				hashMap.put(n.key, 0);
+			}
+		}
+		if (list.size() == 0) {
+			System.out.println("NAO ACHOU");
+			return null;
+		}
+		Collections.sort(list);
+		Node n = list.remove(0);
+		hashMap.put(n.key,1);
+		return n.buscaGulosa(hashMap, list);
+	}
+
+	public int getValorHeuristica() {
+		int v = 0;
+		char[] treearray = Tree.objetivo.toCharArray();
+		char[] nodearray = this.key.toCharArray();
+		for (int i = 0; i < nodearray.length; i++) {
+			if (nodearray[i] != treearray[i]) {
+				if (treearray[i] != '-') {
+					v++;
+				}
+
+			}
+		}
+		return v;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return Integer.compare(this.val, o.val);
 	}
 }
--- src/entity/Tree.java
+++ src/entity/Tree.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -12,6 +13,8 @@ public class Tree {
 	public static String objetivo;
 	public String inicial;
 	public List<Integer> estado;
+	public static int oldIDAPatamar = 0;
+	public static int newIDAPatamar = 0;
 
 	public Tree(String objetivo, String inicial) {
 		this.objetivo = objetivo;
@@ -32,16 +35,122 @@ public class Tree {
 			}
 		}
 		this.raiz = new Node(null, estado);
+		this.raiz.level = 0;
+	}
+
+	public void buscaBacktracking() {
+		List<Node> list = new ArrayList<Node>();
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+		hashMap.put(this.raiz.key, 1);
+		Node n = this.raiz.buscaBacktracking(hashMap, list);
+		printSolution(n, hashMap);
 	}
 
 	public void buscaLargura() {
 		List<Node> list = new ArrayList<Node>();
 		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
-		this.raiz.buscaLargura(hashMap, list);
+		Node n = this.raiz.buscaLargura(hashMap, list);
+		hashMap.put(this.raiz.key, 1);
+		printSolution(n, hashMap);
 	}
+
 	public void buscaProfundidade() {
 		List<Node> list = new ArrayList<Node>();
 		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
-		this.raiz.buscaProfundidade(hashMap, list);
+		hashMap.put(this.raiz.key, 1);
+
+		Node n = this.raiz.buscaProfundidade(hashMap, list);
+		printSolution(n, hashMap);
 	}
+
+	public void buscaOrdenada() {
+		List<Node> list = new ArrayList<Node>();
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+		this.raiz.val = 0;
+		hashMap.put(this.raiz.key, 1);
+		Node n = this.raiz.buscaOrdenada(hashMap, list);
+		printSolution(n, hashMap);
+	}
+
+	public void buscaGulosa() {
+		List<Node> list = new ArrayList<Node>();
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+		this.raiz.val = raiz.getValorHeuristica();
+		hashMap.put(this.raiz.key, 1);
+
+		Node n = this.raiz.buscaGulosa(hashMap, list);
+		printSolution(n, hashMap);
+	}
+
+	public void buscaA() {
+		List<Node> list = new ArrayList<Node>();
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+		this.raiz.val = raiz.getValorHeuristica();
+		hashMap.put(this.raiz.key, 1);
+
+		Node n = this.raiz.buscaA(hashMap, list);
+
+		printSolution(n, hashMap);
+	}
+
+	public void buscaIDA() {
+		List<Node> list = new ArrayList<Node>();
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+
+		this.raiz.val = raiz.getValorHeuristica();
+		this.oldIDAPatamar = this.raiz.val;
+		this.newIDAPatamar = Integer.MAX_VALUE;
+		Node n = null;
+		while (true) {
+			System.out.println("oldIDAPatamar " + oldIDAPatamar);
+			hashMap = new HashMap<String, Integer>();
+			hashMap.put(this.raiz.key, 1);
+			n = this.raiz.buscaIDA(hashMap, list);
+			if (n != null) {
+				break;
+			}
+			if (oldIDAPatamar == newIDAPatamar || newIDAPatamar == Integer.MAX_VALUE) {
+				break;
+			} else {
+				oldIDAPatamar = newIDAPatamar;
+			}
+			this.newIDAPatamar = Integer.MAX_VALUE;
+		}
+
+		printSolution(n, hashMap);
+
+	}
+
+	public void printSolution(Node n, HashMap<String, Integer> hashMap) {
+		if (n == null) {
+			System.out.println("Nao achou Solucao");
+		}
+		List<String> caminho = new ArrayList<String>();
+		System.out.println("Profundidade: " + n.level);
+		System.out.println("Solucao: " + n.key);
+		while (n != null) {
+			caminho.add(n.key);
+			n = n.pai;
+		}
+
+		System.out.println("Solucao:");
+		for (int i = caminho.size() - 1; i >= 0; i--) {
+			System.out.print(caminho.get(i));
+			if (i != 0) {
+				System.out.print(" -> ");
+			}
+		}
+		System.out.println("");
+		int c = 0;
+		Collection<Integer> values = hashMap.values();
+		for (int value : values) {
+			if (value == 1) {
+				c++;
+			}
+		}
+		System.out.println("expandidos: " + hashMap.size());
+		System.out.println("visitados: " + c);
+		System.out.println("do fator de ramificação: " + (float)(hashMap.size()-1)/(c-1));
+	}
+
 }
