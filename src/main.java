94a6c8b030c7969a01f1728eeca1317ea3d3d468
Thiago Costa Rizuti da Rocha <t.rizuti@gmail.com>
2016-07-04 17:10:39 -0300

    nova heuristica

--- src/main.java
+++ src/main.java
@@ -6,10 +6,13 @@ import entity.Tree;
 public class main {
 
 	public  static void main(String[] agr){
-		String estado_inicial = "001-011";
+		String estado_inicial = "10-0101";
 		String estado_final = "000-111";
 		Tree tree = new Tree(estado_final,estado_inicial);
-		tree.buscaBacktracking();
+		long tempoInicial = System.currentTimeMillis();
+                tree.buscaLargura();
+                long tempoFinal = System.currentTimeMillis();
+                System.out.println(tempoFinal - tempoInicial + "ms");
 	}
 
 
